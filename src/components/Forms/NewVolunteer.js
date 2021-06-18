979cbc03464c458cd6054a8df2f900d3805f3893
Joshua Perez Leduc <joshuapleduc@gmail.com>
2021-06-17 23:54:29 -0400

    start forms

--- src/components/Forms/NewVolunteer.js
+++ src/components/Forms/NewVolunteer.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function NewVolunteer() {
-  return <div></div>;
+  const [name, age, email, phone, address, addressTwo, zipcode, city, state, sign] = useRef(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log();
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input type="text" ref={name} />
+        <br></br>
+        <label>Age:</label>
+        <input type="number" ref={age} />
+        <br></br>
+        <label>E-mail:</label>
+        <input type="email" ref={email} />
+        <br></br>
+        <label>Phone:</label>
+        <input type="tel" ref={phone} />
+        <br></br>
+        <label>Address:</label>
+        <input type="text" ref={address} />
+        <br></br>
+        <label>Address(line 2):</label>
+        <input type="text" ref={addressTwo} />
+        <br></br>
+        <label>Zipcode:</label>
+        <input type="number" ref={zipcode} />
+        <br></br>
+        <label>City:</label>
+        <input type="text" ref={city} />
+        <br></br>
+        <label>State:</label>
+        <input type="text" ref={state} />
+        <br></br>
+        <label>Sign:</label>
+        <input type="text" ref={sign} />
+        <br></br>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default NewVolunteer;
--- src/pages/Volunteers.js
+++ src/pages/Volunteers.js
@@ -1,44 +1,11 @@
+import { NewVolunteer } from "components";
 import React from "react";
 
 function Volunteers() {
   return (
     <div>
       Volunteers
-      <div>
-        <form>
-          <lable>Name:</lable>
-          <input type="text" />
-          <br></br>
-          <lable>Age:</lable>
-          <input type="number" />
-          <br></br>
-          <lable>E-mail:</lable>
-          <input type="email" />
-          <br></br>
-          <lable>Phone:</lable>
-          <input type="tel" />
-          <br></br>
-          <lable>Address:</lable>
-          <input type="text" />
-          <br></br>
-          <lable>Address(line 2):</lable>
-          <input type="text" />
-          <br></br>
-          <lable>Zipcode:</lable>
-          <input type="number" />
-          <br></br>
-          <lable>City:</lable>
-          <input type="text" />
-          <br></br>
-          <lable>State:</lable>
-          <input type="text" />
-          <br></br>
-          <lable>Sign:</lable>
-          <input type="text" />
-          <br></br>
-          <button>Submit</button>
-        </form>
-      </div>
+      <NewVolunteer />
     </div>
   );
 }
