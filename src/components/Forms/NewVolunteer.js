61e0f9b967a923b3b602a539dcdb366d10bdf199
Joshua Perez Leduc <joshuapleduc@gmail.com>
2021-06-18 13:34:05 -0400

    add form controls to volunteer form

--- src/components/Forms/NewVolunteer.js
+++ src/components/Forms/NewVolunteer.js
@@ -1,43 +1,59 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 
 function NewVolunteer() {
-  const [name, age, email, phone, address, addressTwo, zipcode, city, state, sign] = useRef(null);
+  const [volunteer, setVolunteer] = useState({
+    name: "",
+    age: "",
+    email: "",
+    phone: "",
+    address: "",
+    addressTwo: "",
+    zipcode: "",
+    city: "",
+    state: "",
+    sign: "",
+  });
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log();
+    console.log(volunteer);
+  };
+  const handleChange = (e) => {
+    setVolunteer((prevValues) => {
+      return { ...prevValues, [e.target.name]: e.target.value };
+    });
   };
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <label>Name:</label>
-        <input type="text" ref={name} />
+        <input type="text" name="name" value={volunteer.name} onChange={handleChange} required="required" placeholder="First and Last Name" />
         <br></br>
         <label>Age:</label>
-        <input type="number" ref={age} />
+        <input type="number" name="age" value={volunteer.age} onChange={handleChange} required="required" />
         <br></br>
         <label>E-mail:</label>
-        <input type="email" ref={email} />
+        <input type="email" name="email" value={volunteer.email} onChange={handleChange} required="required" placeholder="example@email.com" />
         <br></br>
         <label>Phone:</label>
-        <input type="tel" ref={phone} />
+        <input type="tel" name="phone" value={volunteer.phone} onChange={handleChange} required="required" />
         <br></br>
         <label>Address:</label>
-        <input type="text" ref={address} />
+        <input type="text" name="address" value={volunteer.address} onChange={handleChange} required="required" />
         <br></br>
         <label>Address(line 2):</label>
-        <input type="text" ref={addressTwo} />
+        <input type="text" name="addressTwo" value={volunteer.addressTwo} onChange={handleChange} />
         <br></br>
         <label>Zipcode:</label>
-        <input type="number" ref={zipcode} />
+        <input type="number" name="zipcode" value={volunteer.zipcode} onChange={handleChange} required="required" />
         <br></br>
         <label>City:</label>
-        <input type="text" ref={city} />
+        <input type="text" name="city" value={volunteer.city} onChange={handleChange} required="required" />
         <br></br>
         <label>State:</label>
-        <input type="text" ref={state} />
+        <input type="text" name="state" value={volunteer.state} onChange={handleChange} required="required" />
         <br></br>
         <label>Sign:</label>
-        <input type="text" ref={sign} />
+        <input type="text" name="sign" value={volunteer.sign} onChange={handleChange} required="required" />
         <br></br>
         <button>Submit</button>
       </form>
